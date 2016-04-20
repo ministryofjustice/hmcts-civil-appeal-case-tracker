4e86a70ff7123e84429c6aedcbce37673ce4a0c4
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-04-20 14:57:15 +0100

    WIP - Working Build folder can be removed once static assets or gulp is working properly

---
+++ Working_build/WEB-INF/src/com/calander/actions/searchAction.java
@@ -0,0 +1,52 @@
+package com.calander.actions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.calander.plugin.HibernatePlugin;
+
+public class searchAction extends Action {
+	 public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+     throws IOException, ServletException, Exception
+ {
+		 String strQry;
+		 List arrResults=null;
+		 strQry= (String) request.getParameter("search");
+	
+		 	Session session = null;
+			SessionFactory factory=null;
+			Query qry=null;
+			
+			//getting session object from Hibernate Util class	
+			factory = (SessionFactory) servlet.getServletContext().getAttribute(HibernatePlugin.KEY_NAME);
+			session = factory.openSession();
+		 
+	
+		 	qry = session.createQuery("from Calander c where c.search_date like :date or c.case_no like :case or title1 like :title order by c.case_no");
+		 	qry.setString("date","%"+strQry+"%");
+		 	qry.setString("case","%"+strQry+"%");
+		 	qry.setString("title","%"+strQry+"%");
+		 	
+		 	arrResults = qry.list();
+		 	
+		 	session.clear();
+		 	session.close();
+		 	request.getSession().setAttribute("results", arrResults);
+		 	
+		 return mapping.findForward("success");
+ }
+
+}
