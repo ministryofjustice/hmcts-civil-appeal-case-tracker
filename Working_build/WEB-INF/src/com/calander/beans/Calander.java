a42ede498d6216d35334c6ba16d04bcce07dbd39
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-04-20 14:57:15 +0100

    WIP - Working Build folder can be removed once static assets or gulp is working properly

---
+++ Working_build/WEB-INF/src/com/calander/beans/Calander.java
@@ -0,0 +1,192 @@
+package com.calander.beans;
+
+public class Calander {
+
+	private int case_id;
+	private String search_date;
+	private String case_no;
+	private String heading_status;
+	private String judge1;
+	private String judge2;
+	private String judge3;
+	private String lcourt;
+	private String venue;
+	private String case_ref;
+	private String title1;
+	private String title2;
+	private String type;
+	private String lc_judge;
+	private String nature;
+	private String last_updated;
+	private String result;
+	private String status;
+	private String track_line1;
+	private String track_line2;
+	private String track_line3;
+	private String track_line4;
+	private String track_line5;
+	private String track_line6;
+	private String track_line7;
+	private String track_line8;
+	
+	public int getCase_id() {
+		return case_id;
+	}
+	public void setCase_id(int case_id) {
+		this.case_id = case_id;
+	}
+	public String getSearch_date() {
+		return search_date;
+	}
+	public void setSearch_date(String search_date) {
+		this.search_date = search_date;
+	}
+	public String getCase_no() {
+		return case_no;
+	}
+	public void setCase_no(String case_no) {
+		this.case_no = case_no;
+	}
+	public String getHeading_status() {
+		return heading_status;
+	}
+	public void setHeading_status(String heading_status) {
+		this.heading_status = heading_status;
+	}
+	public String getJudge1() {
+		return judge1;
+	}
+	public void setJudge1(String judge1) {
+		this.judge1 = judge1;
+	}
+	public String getJudge2() {
+		return judge2;
+	}
+	public void setJudge2(String judge2) {
+		this.judge2 = judge2;
+	}
+	public String getJudge3() {
+		return judge3;
+	}
+	public void setJudge3(String judge3) {
+		this.judge3 = judge3;
+	}
+	public String getLcourt() {
+		return lcourt;
+	}
+	public void setLcourt(String lcourt) {
+		this.lcourt = lcourt;
+	}
+	public String getVenue() {
+		return venue;
+	}
+	public void setVenue(String venue) {
+		this.venue = venue;
+	}
+	public String getCase_ref() {
+		return case_ref;
+	}
+	public void setCase_ref(String case_ref) {
+		this.case_ref = case_ref;
+	}
+	public String getTitle1() {
+		return title1;
+	}
+	public void setTitle1(String title1) {
+		this.title1 = title1;
+	}
+	public String getTitle2() {
+		return title2;
+	}
+	public void setTitle2(String title2) {
+		this.title2 = title2;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getLc_judge() {
+		return lc_judge;
+	}
+	public void setLc_judge(String lc_judge) {
+		this.lc_judge = lc_judge;
+	}
+	public String getNature() {
+		return nature;
+	}
+	public void setNature(String nature) {
+		this.nature = nature;
+	}
+	public String getLast_updated() {
+		return last_updated;
+	}
+	public void setLast_updated(String last_updated) {
+		this.last_updated = last_updated;
+	}
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getTrack_line1() {
+		return track_line1;
+	}
+	public void setTrack_line1(String track_line1) {
+		this.track_line1 = track_line1;
+	}
+	public String getTrack_line2() {
+		return track_line2;
+	}
+	public void setTrack_line2(String track_line2) {
+		this.track_line2 = track_line2;
+	}
+	public String getTrack_line3() {
+		return track_line3;
+	}
+	public void setTrack_line3(String track_line3) {
+		this.track_line3 = track_line3;
+	}
+	public String getTrack_line4() {
+		return track_line4;
+	}
+	public void setTrack_line4(String track_line4) {
+		this.track_line4 = track_line4;
+	}
+	public String getTrack_line5() {
+		return track_line5;
+	}
+	public void setTrack_line5(String track_line5) {
+		this.track_line5 = track_line5;
+	}
+	public String getTrack_line6() {
+		return track_line6;
+	}
+	public void setTrack_line6(String track_line6) {
+		this.track_line6 = track_line6;
+	}
+	public String getTrack_line7() {
+		return track_line7;
+	}
+	public void setTrack_line7(String track_line7) {
+		this.track_line7 = track_line7;
+	}
+	public String getTrack_line8() {
+		return track_line8;
+	}
+	public void setTrack_line8(String track_line8) {
+		this.track_line8 = track_line8;
+	}
+	
+	
+	
+	
+}
---
+++ Working_build/includes/cookie.jsp
@@ -0,0 +1,12 @@
+<div id="skiplink-container">
+  <div>
+    <a href="#content" class="skiplink">Skip to main content</a>
+  </div>
+</div>
+
+<div id="global-cookie-message">
+
+    <p>GOV.UK uses cookies to make the site simpler. <a href="https://www.gov.uk/help/cookies">Find out more about cookies</a></p>
+
+</div>
+<!--end global-cookie-message-->
