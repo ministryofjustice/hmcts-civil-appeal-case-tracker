0322126ea6c297398ace24815c4cd063fed79b3f
paul.crimes <paul.crimes@hmcts.net>
2021-12-11 15:55:48 +0000

    replaced log4j with v 2.15 and removed build artifacts and moved source files to better directories

---
+++ WEB-INF/src/com/calander/util/CsvImportJob.java
@@ -0,0 +1,240 @@
+package com.calander.util;
+
+
+import au.com.bytecode.opencsv.CSVReader;
+import com.calander.beans.Calander;
+import com.calander.plugin.HibernatePlugin;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+
+public class CsvImportJob implements Job {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CsvImportJob.class);
+
+    public static void main(String args[]) throws Exception {
+        downloadCsvFromS3BucketAndReplaceDatabaseWithContents();
+    }
+
+
+    public void execute(JobExecutionContext arg0)
+        throws JobExecutionException {
+        downloadCsvFromS3BucketAndReplaceDatabaseWithContents();
+    }
+
+
+    public static void downloadCsvFromS3BucketAndReplaceDatabaseWithContents() {
+
+        int rowCount = 0;
+        HibernatePlugin hibernatePlugin = new HibernatePlugin();
+        SessionFactory factory = null;
+        try {
+            factory = (SessionFactory) hibernatePlugin.getconnection();
+        } catch (Exception e) {
+            throw new RuntimeException("Hibernate Plugin unable to get connection");
+        }
+        Session session = factory.openSession();
+
+        try {
+
+            String result = null;
+
+            String url = "https://case-tracker.s3.eu-west-2.amazonaws.com/data.csv";
+
+            URL u;
+            InputStream is = null;
+
+
+            u = new URL(url);
+            is = u.openStream();
+            BufferedReader reader1 = new BufferedReader(new InputStreamReader(is));
+
+            CSVReader reader = new CSVReader(reader1);
+            System.out.println("coming in run schedular>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+            String[] nextLine;
+            Calander calander = null;
+
+
+            session.beginTransaction();
+            session.createQuery("delete Calander").executeUpdate();
+//            session.getTransaction().commit();
+//            session.beginTransaction();
+            rowCount = populateRows(rowCount, session, reader);
+            session.getTransaction().commit();
+            LOGGER.info("Success {} rows added in database", rowCount);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            session.getTransaction().rollback();
+            LOGGER.error("Import Failed: {}", ex.getMessage());
+            session.flush();
+            session.clear();
+            session.close();
+            //TODO send alert to slack or have a healthcheck page that shows last time db was updated
+        } finally {
+
+            session.flush();
+            session.clear();
+            session.close();
+        }
+        System.out.println("Scheduler Finished");
+
+    }
+
+
+
+
+    private static int populateRows(int rowCount, Session session, CSVReader reader) throws IOException {
+        String[] nextLine;
+        Calander calander;
+        while ((nextLine = reader.readNext()) != null) {
+            LOGGER.info("Processing Row {}", rowCount);
+
+            int line_length = nextLine.length;
+            int column = 0;
+
+            calander = new Calander();
+
+            if (column < line_length) {
+                calander.setSearch_date(nextLine[0]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setCase_no(nextLine[1]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setHeading_status(nextLine[2]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setJudge1(nextLine[3]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setJudge2(nextLine[4]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setJudge3(nextLine[5]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setLcourt(nextLine[6]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setVenue(nextLine[7]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setCase_ref(nextLine[8]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setTitle1(nextLine[9] + " " + nextLine[10]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setTitle2(nextLine[10]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setType(nextLine[11]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setLc_judge(nextLine[12]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setNature(nextLine[13]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setLast_updated(nextLine[14]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setResult(nextLine[15]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setStatus(nextLine[16]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setTrack_line1(nextLine[17]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setTrack_line2(nextLine[18]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setTrack_line3(nextLine[19]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setTrack_line4(nextLine[20]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setTrack_line5(nextLine[21]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setTrack_line6(nextLine[22]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setTrack_line7(nextLine[23]);
+            }
+            column++;
+
+            if (column < line_length) {
+                calander.setTrack_line8(nextLine[24]);
+            }
+
+            rowCount++;
+            session.save(calander);
+
+        }
+        return rowCount;
+    }
+
+
+}
--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-
 #!/bin/bash
-ant
+docker run  --mount type=bind,source="$(pwd)",target=/dockerrun/  paulushc/apacheant ant -buildfile /dockerrun/ clean build
 docker build -t cact .
+echo "to run:     docker run -p 8080:8080 --name cact cact"
+echo "to remove:  docker container rm cact"
+echo
+echo "or use docker compose up"
