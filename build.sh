2fb7da6354d14c633d1674b7f7d17663e836abe2
paul.crimes <paul.crimes@hmcts.net>
2021-12-13 18:17:04 +0000

    add slf4j adapter and latest log4j 1.x

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 docker run  --mount type=bind,source="$(pwd)",target=/dockerrun/  paulushc/apacheant ant -buildfile /dockerrun/ clean build
 docker build -t cact .
-echo "to run:     docker run -p 8080:8080 --name cact cact"
-echo "to remove:  docker container rm cact"
-echo
-echo "or use docker compose up"
