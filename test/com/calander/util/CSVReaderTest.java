d8e273fa7fdb46569e5795f1617ff74b893cfad2
Matthew Searle <65017209+matthewsearle01@users.noreply.github.com>
2024-09-12 15:46:29 +0100

    add additional unit test to ensure csv reader handles escaped quotations correctly

--- test/com/calander/util/CSVReaderTest.java
+++ test/com/calander/util/CSVReaderTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import au.com.bytecode.opencsv.CSVReader;
 import java.io.StringReader;
+import java.io.IOException;
 
 public class CSVReaderTest {
 
@@ -25,5 +26,18 @@ public class CSVReaderTest {
         assertEquals("value1", parsedLine[0]);
         assertEquals("value2", parsedLine[1]);
         assertEquals("value3", parsedLine[2]);
+    }
+
+    @Test
+    public void testParseLineWithEscapedQuotes() throws IOException {
+        String input = "\"Column 1\",\"Column \\\"2\\\"\",\"Column 3\"";
+        CSVReader reader = new CSVReader(new StringReader(input));
+        
+        String[] result = reader.readNext();
+        assertNotNull(result);
+        assertEquals(3, result.length);
+        assertEquals("Column 1", result[0]);
+        assertEquals("Column \"2\"", result[1]);
+        assertEquals("Column 3", result[2]);
     }
 }
