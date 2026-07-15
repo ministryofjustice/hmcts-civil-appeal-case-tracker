01116a354c1a4cb1a453c60322fb9eae38dcc49e
Timi Olubusi <timiolubusi@icloud.com>
2026-07-15 16:10:44 +0100

    Adding case Details page tests

---
+++ e2e/tests/content/caseDetailsPage_content.ts
@@ -0,0 +1,9 @@
+const caseDetailsPage_content = {
+    pageTitle: "Case Tracker for Civil Appeals",
+    sectionHeadingPrefix: "Summary Case Details for",
+    caseHeading: "Case",
+    caseResultsHeading: "Case results:",
+    trackYourCaseHeading: "Track Your Case:",
+    trackingInformationHeading: "Tracking Information:"
+} as const;
+export default caseDetailsPage_content
--- e2e/tests/find-case.spec.ts
+++ e2e/tests/find-case.spec.ts
@@ -2,6 +2,12 @@ import {test} from "@playwright/test";
 import LandingPage from "./pages/landingPage";
 import SearchPage from "./pages/searchPage";
 import details from "./data";
+import createCaseInDatabaseFromCsv from "./helpers/createCaseInDatabaseFromCsv";
+import CaseDetailsPage from "./pages/caseDetailsPage";
+
+test.beforeAll('Admin uploads csv to database', async () => {
+    await createCaseInDatabaseFromCsv();
+});
 
 test('Find a case via calendar and go to Summary Case Details', async ({page}) => {
     const landingPage: LandingPage = new LandingPage();
@@ -12,7 +18,9 @@ test('Find a case via calendar and go to Summary Case Details', async ({page}) =
     await searchPage.checkPageLoads(page);
     await searchPage.selectDate(page, details.searchDate.day, details.searchDate.month, details.searchDate.year); // clicks calendar icon, picks the day
     await searchPage.search(page);
+    await searchPage.selectRecordAndContinueOn(page, details.searchCaseReference.caseRef2);
 
-    await page.pause();
-
+    const casePage: CaseDetailsPage = new CaseDetailsPage();
+    await casePage.checkPageLoads(page);
+    await casePage.checkCaseDetails(page, details.caseDetails.caseDetails2);
 });
