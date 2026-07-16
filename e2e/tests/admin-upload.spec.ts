185669cd1cc73b754c7079f680d84eef19c6d77c
Timi Olubusi <timiolubusi@icloud.com>
2026-07-16 15:24:54 +0100

    Wrapping test suites in a describe with tags

--- e2e/tests/admin-upload.spec.ts
+++ e2e/tests/admin-upload.spec.ts
@@ -3,21 +3,23 @@ import path from 'path';
 import AdminLoginPage from './pages/adminLoginPage';
 import AdminUploadPage from './pages/adminUploadPage';
 
-test('Admin can upload a CSV and import it into the database', async ({ page }) => {
-    const adminUser = process.env.ADMIN_USER;
-    const adminPass = process.env.ADMIN_PASS;
+test.describe('Admin Upload',  { tag: '@e2e' }, () => {
+    test('Admin can upload a CSV and import it into the database', async ({ page }) => {
+        const adminUser = process.env.ADMIN_USER;
+        const adminPass = process.env.ADMIN_PASS;
 
-    if (!adminUser || !adminPass) {
-        throw new Error('ADMIN_USER and ADMIN_PASS env vars must be set to run this test');
-    }
+        if (!adminUser || !adminPass) {
+            throw new Error('ADMIN_USER and ADMIN_PASS env vars must be set to run this test');
+        }
 
-    const loginPage = new AdminLoginPage();
-    await loginPage.checkPageLoads(page);
-    await loginPage.loginAndContinueOn(page, adminUser, adminPass);
+        const loginPage = new AdminLoginPage();
+        await loginPage.checkPageLoads(page);
+        await loginPage.loginAndContinueOn(page, adminUser, adminPass);
 
-    const uploadPage = new AdminUploadPage();
-    await uploadPage.checkPageLoads(page);
-    await uploadPage.uploadFile(page, path.join(__dirname, 'data', 'CASE_TRACKER.csv'));
-    await uploadPage.importIntoDatabase(page);
-    await uploadPage.checkImportMessage(page, '10 rows added in database');
+        const uploadPage = new AdminUploadPage();
+        await uploadPage.checkPageLoads(page);
+        await uploadPage.uploadFile(page, path.join(__dirname, 'data', 'CASE_TRACKER.csv'));
+        await uploadPage.importIntoDatabase(page);
+        await uploadPage.checkImportMessage(page, '10 rows added in database');
+    });
 });
--- e2e/tests/find-case.spec.ts
+++ e2e/tests/find-case.spec.ts
@@ -5,22 +5,25 @@ import details from "./data";
 import createCaseInDatabaseFromCsv from "./helpers/createCaseInDatabaseFromCsv";
 import CaseDetailsPage from "./pages/caseDetailsPage";
 
-test.beforeAll('Admin uploads csv to database', async () => {
-    await createCaseInDatabaseFromCsv();
-});
+test.describe('User journey to find case details',  { tag: '@e2e' }, () => {
+    test.beforeAll('Admin uploads csv to database', async () => {
+        await createCaseInDatabaseFromCsv();
+    });
 
-test('Find a case via calendar and go to Summary Case Details', async ({page}) => {
-    const landingPage: LandingPage = new LandingPage();
-    await landingPage.checkPageLoads(page);
-    await landingPage.continueOn(page);
+    test('Find a case via calendar and go to Summary Case Details', async ({page}) => {
+        const landingPage: LandingPage = new LandingPage();
+        await landingPage.checkPageLoads(page);
+        await landingPage.continueOn(page);
 
-    const searchPage = new SearchPage();
-    await searchPage.checkPageLoads(page);
-    await searchPage.selectDate(page, details.searchDate.day, details.searchDate.month, details.searchDate.year); // clicks calendar icon, picks the day
-    await searchPage.search(page);
-    await searchPage.selectRecordAndContinueOn(page, details.searchCaseReference.caseRef2);
+        const searchPage = new SearchPage();
+        await searchPage.checkPageLoads(page);
+        await searchPage.selectDate(page, details.searchDate.day, details.searchDate.month, details.searchDate.year); // clicks calendar icon, picks the day
+        await searchPage.search(page);
+        await searchPage.selectRecordAndContinueOn(page, details.searchCaseReference.caseRef2);
 
-    const casePage: CaseDetailsPage = new CaseDetailsPage();
-    await casePage.checkPageLoads(page);
-    await casePage.checkCaseDetails(page, details.caseDetails.caseDetails2);
+        const casePage: CaseDetailsPage = new CaseDetailsPage();
+        await casePage.checkPageLoads(page);
+        await casePage.checkCaseDetails(page, details.caseDetails.caseDetails2);
+    });
+
 });
