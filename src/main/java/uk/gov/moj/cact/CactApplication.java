53611303ed1d82018815ec858776273d45b1099e
Timi Olubusi <timiolubusi@icloud.com>
2026-07-17 17:54:39 +0100

    Gradle spring boot via spring initialzr

---
+++ src/main/java/uk/gov/moj/cact/CactApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.moj.cact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gov/moj/cact/CactApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.moj.cact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
