56fc57f5604b9ecc1ddc2df8213a28d0ff745a53
Timi Olubusi <timiolubusi@icloud.com>
2026-07-23 11:38:54 +0100

    Adding enable scheduling & base controller for returning index thats ported over

--- src/main/java/uk/gov/moj/cact/CactApplication.java
+++ src/main/java/uk/gov/moj/cact/CactApplication.java
@@ -2,8 +2,10 @@ package uk.gov.moj.cact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CactApplication {
 
 	public static void main(String[] args) {
